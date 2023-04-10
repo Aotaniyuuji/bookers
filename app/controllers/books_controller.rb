a17db4e895aaf4dcb4081c0ed12108df9e930087
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-10 10:42:13 +0000

    [update]課題 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(list.id)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
