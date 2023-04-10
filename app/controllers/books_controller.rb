80576747b669bdfcb35394a15d4db2a6d8675879
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-10 11:40:52 +0000

    [update]課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
 
-    book.save
-
-    redirect_to '/books'
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def show
