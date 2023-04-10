7916978fae01ee3bc290a5cfce2995c61e6a1c4e
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-04-10 07:51:49 +0000

    [update]課題2

---
+++ db/migrate/20230410073340_addid_tobooks.rb
@@ -0,0 +1,4 @@
+class AddidTobooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_09_034738) do
+ActiveRecord::Schema.define(version: 2023_04_10_073340) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
